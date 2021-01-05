df222fcdc299f3a4603fff37a406d544a3d11749
Ugin-gt <apach575@gmail.com>
2021-01-05 18:44:53 +0200

    Add operator let, const, alert, console.log, if & else for the first JS sample

---
+++ index.js
@@ -0,0 +1,44 @@
+// var // не используется
+// const
+
+let box; // Объявление переменной
+box = 20; // Инициализация переменной
+console.log('box=', typeof box, box); // Логирование переменной
+
+let importantValue;
+importantValue = 'Главная строка';
+console.log('importantValue=', typeof importantValue, importantValue);
+
+let value;
+value = 30;
+console.log('value=', typeof value, value);
+
+value = true;
+console.log('value=', typeof value, value);
+
+const num1 = 10;
+const num2 = 35;
+
+console.log(num1 + num2);
+
+console.log(num1 - num2);
+
+console.log(1);
+
+const result = false;
+if (result) {
+  console.log('ok');s
+} else {
+  console.log('not ok')
+}
+console.log(3);
+
+
+
+
+
+console.log('Я джава девелопер!');
+console.log(undefined);
+console.log(null);
+console.log(true);
+console.log(false);
