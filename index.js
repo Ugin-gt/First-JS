bcfe0499c704b1bbdad5dd79746cdcd561d632cc
Ugin-gt <apach575@gmail.com>
2021-01-07 15:11:21 +0200

    Configurate 'getSumOfTwo' function expression

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const userInput1 = +prompt('Введите первое число:');
 const userInput2 = +prompt('Введите второе число:');
 
-const getSumOfTwo = function () {
+const getSumOfTwo = function (num1, num2) {
   //function expression
 
-  const result = userInput1 + userInput2;
- 
-  return result;
-  };
+  const result = num1 + num2;
+  if (!isNaN(result)) {
+    return result;
+  }
+  return false;
+};
 
-const summ = getSumOfTwo();
-console.log(summ); 
+const summ = getSumOfTwo(userInput1, userInput2);
+
+if (!summ) {
+  alert('Введите правильно Ваши числа');
+} else {
+  console.log(summ);
+};
+
