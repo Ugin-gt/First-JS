e381d02e2908f5450593c21dce3e567d427c4c87
Ugin-gt <apach575@gmail.com>
2021-01-07 14:40:29 +0200

    Modify JS function expression to get  summ

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-const a = 5;
-const b = 7;
-console.log(a * b);
-
-const c = 32;
-const d = 4;
-console.log(c / d);
-
-const e = 12;
-const f = 4;
-console.log(e + f);
-
-const number = 11;
-const boolean = true;
-const string = 'Java Script';
-const stringNum = '100';
-console.log(number, boolean, string, stringNum);
-
-let resultN = 1;
-
-console.log(resultN += 11);
-console.log(resultN -= 11);
-console.log(resultN *= 11);
-console.log(resultN /= 11);
-console.log(resultN **= 2);
-console.log(resultN += 1);
-console.log(resultN -= 1);
-
-// const userInput = prompt('Введите число:');
-
-// alert('Вы ввели число: ' + userInput);
+const userInput1 = +prompt('Введите первое число:');
+const userInput2 = +prompt('Введите второе число:');
 
 const getSumOfTwo = function () {
+  //function expression
 
-  const userInputNumber1 = +prompt('Введите первое число:');
-  const userInputNumber2 = +prompt('Введите второе число:');
+  const result = userInput1 + userInput2;
+ 
+  return result;
+  };
 
-  const result = userInputNumber1 + userInputNumber2;
-  const verdict = isNaN(result);
-
-  if (!verdict) {
-    alert('Сумма чисел равна = ' + result);
-  } else {
-    alert('Ошибка! Вы ввели не число.');
-  }
-};
-
-getSumOfTwo();
+const summ = getSumOfTwo();
+console.log(summ); 
