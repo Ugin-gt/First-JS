41979b5822b34a5823c15bd451ac8e848473d1c4
Ugin-gt <apach575@gmail.com>
2021-01-08 01:13:56 +0200

    Add if-else script construction

--- index.js
+++ index.js
@@ -1,89 +1,25 @@
-const a = 15;
-const b = 20;
-const number = 15;
+const userIput = +prompt('Введите число: ');
 
-const minValue = function (a, b) {
-  if (a < b) {
-    return a;
-  } else {
-    return b;
-  }
-};
+if (isNaN(userIput)) {
 
-console.log(minValue(a, b));
+  console.log('Вы ввели не число');
 
-const maxValue = function (a, b) {
-  if (a > b) {
-    return a;
-  } else {
-    return b;
-  }
-};
+} else if (userIput % 5 === 0) {
 
-console.log(maxValue(a, b));
+  console.log('Делится на 5 без остатка');
 
-const isEven = function (number) {
-  if (isNaN(number)) {
-    return "is NaN";
-  }
-  return number % 2 === 0;
-};
+} else if (userIput % 3 === 0) {
 
-console.log(isEven(number));
+  console.log('Делится на 3 без остатка');
 
-/**
- * 
- * @param {number} a - first coef
- * @param {number} b - second coef
- * @param {number} c - third coef
- * @returns {null | number []} - result
- */
+} else if (userIput % 2 === 0) {
 
-const solveSquareEquation = function (a, b, c,) {
-  const checkValues = a - b - c;
-  if (isNaN(checkValues)) {
-    return null;
-  }
+  console.log('Делится на 2 без остатка');
 
-  const d = b * b - 4 * a * c;
+} else {
 
-  if (d < 0) {
-    return null;
-  }
-  if (d === 0) {
-    const x = -b / (2 * a);
-    return [x, x];
-  }
-  if (d > 0) {
-    const dSqrt = d ** (1 / 2);
+  console.log('Не делится ни на 5, ни на 3, ни на 2')
 
-    const x1 = (-b + dSqrt) / (2 * a);
-    const x2 = (-b - dSqrt) / (2 * a);
-
-    return [x1, x2];
-  }
-};
-
-const result = solveSquareEquation(2, 7, 4);
-
-console.log(result);
-
-const globalVar = 'Global';
-//  глобальные переменные видны в любой части кода;
-
-if (globalVar) {
-  const ifVar1 = 'ifVar1';
-
-  console.log(ifVar1);
-  console.log(globalVar);
-
-  if (globalVar) {
-    const ifVar2 = 'ifVar2';
-    
-    console.log(ifVar2);
-    console.log(ifVar1);
-    console.log(globalVar);
-
-  }
 }
-console.log(globalVar);
+
+console.log('end of script');
