263a24be71e40cf3b511b8e3467a9b77414f7b4c
Ugin-gt <apach575@gmail.com>
2021-01-08 21:32:01 +0200

    Add continue to while operator

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 
-let count =0;
+let count = 0;
 
 while (true) {
+
+  count++;
+
+  if (count % 2 === 1) {
+    continue;
+  }
   console.log(count);
-  if (++count >10) {
+
+
+  if (count > 10) {
     break;
   }
 };
