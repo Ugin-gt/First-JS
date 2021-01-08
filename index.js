cf0f8b6905c4a43aed401c55be63413aaff0604f
Ugin-gt <apach575@gmail.com>
2021-01-08 17:06:28 +0200

    Add conditional (ternary) operator

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-const sum = function (a, b) {
-  return a + b;
-};
-const subtraction = function (a, b) {
-  return a - b;
-};
-const mult = function (a, b) {
-  return a * b;
-};
-const division = function (a, b) {
-  return a / b;
+const getUserInput = function () {
+  const userInput = prompt('Введите любое значение: ');
+
+  const returnValue = userInput ? userInput : 'Вы ничего не ввели';
+  return returnValue;
 };
 
-const userInput1 = +prompt('Введите 1-е число:');
-const operation = prompt('Введите знак операции:');
-const userInput2 = +prompt('Введите 2-е число:');
+
+//   if (userInput) {
+//     return userInput;
+
+//   } return 'Вы ничего не ввели';
+// };
+
+const result = getUserInput();
+
+console.log(result);
 
 
-const calculate = function (a, b, sign) {
-  const checkInput = a - b;
-  if (isNaN(checkInput)) {
-    return null;
-  }
-  switch (sign) {
-    case '+': {
-      return sum(a, b);
-    }
-    case '-': {
-      return subtraction(a, b);
-    }
-    case '*': {
-      return mult(a, b);
-    }
-    case '/': {
-      return division(a, b);
-    }
-    default: {
-      return null;
-      
-    }
-  }
 
-}
-
-const result = calculate(userInput1, userInput2, operation);
-
-console.log('Результат операции ' + result);
 
