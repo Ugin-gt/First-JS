6d35ce19592f3416eb2f9788c254ed9bbe1ab653
Ugin-gt <apach575@gmail.com>
2021-01-09 22:52:40 +0200

    Add function constructor 'CCCC'at

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
 'use strict';
 
-function degree(base, numberOfDegree) {
-  if (numberOfDegree < 0) { // for negative degree
-    return 1 / degree(base, -numberOfDegree);
-
-  }
-  if (numberOfDegree === 0) {
-    return 1;
-  }
-  if (numberOfDegree === 1) {
-    return base;
-  }
-  let result = 1; // отдельная переменная для вывода результата работы функции
-
-  for (let i = 0; i < numberOfDegree; ++i) {
-    result *= base;
-  }
-
-  return result;
-};
-
 //Обьекты в JS, значения и свойства объектов
-
-const cat={
-  color: 'black',
-  breed: 'Spinx',
-  age: 8,
-  isMale: true,
-  name:'Slide',
-};
-
-console.log(cat);
+// Функция  Constructor всегда начинается с большой буквы
 
 
-cat.age = cat.age +1; // изменили  существующее свойство на основе существующего
-console.log(cat);
+function Cat(name, color, age, isMale,breed) {
+  this.name =name;
+  this.furColor = color;
+  this.age = age;
+  this.isMale= isMale;
+  this.breed =breed;
+}
 
-cat.name = 'Test Testovich'; // изменили существующее свойство name
-console.log(cat);
-
-cat.amountOfLegs = 4; // создали новое свойство
-console.log(cat);
-
-delete cat.amountOfLegs //  удалили внесенное свойство
-console.log(cat);
-
+const newCat1 = new Cat ('Murzik','Red',5, true, 'Persian');
+const newCat2 = new Cat ('Sofa','White', 12 , false, 'Spinx');
+const newCat3 = new Cat ('Bernard','Red',3, true, null);
