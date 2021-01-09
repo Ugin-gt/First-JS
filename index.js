b46bf41812cc820a8a1890f25faf2be5a8c25515
Ugin-gt <apach575@gmail.com>
2021-01-09 16:07:02 +0200

    Add factorial <0 return false;

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 const inputDigitFactorial = +prompt('Введите число для вычисления факториала: ');
 
 function factorial(n) {
-  if (!isNaN(n)) {
+
+  if (!isNaN(n), !(n < 0)) {
     return n ? n * factorial(n - 1) : 1
   }
   else {
-    alert('Вы ввели не число!');
+    alert('Неверный ввод или число < 0');
+    return false;
   }
 };
 
 alert(inputDigitFactorial + '! = ' + factorial(inputDigitFactorial));
+
 
 
