0645e230bf787cf1a4764af55ada1ebe72f76fb4
Ugin-gt <apach575@gmail.com>
2021-01-09 23:19:23 +0200

    Add this.sleep & eating to Cat function constructor

--- index.js
+++ index.js
@@ -4,14 +4,21 @@
 // Функция  Constructor всегда начинается с большой буквы
 
 
-function Cat(name, color, age, isMale,breed) {
-  this.name =name;
+function Cat(name, color, age, isMale, breed) {
+  this.name = name;
   this.furColor = color;
   this.age = age;
-  this.isMale= isMale;
-  this.breed =breed;
+  this.isMale = isMale;
+  this.breed = breed;
+  this.sleep = function () {
+    return "I'm sleeping";
+  };
+  this.eat = function () {
+    return "I'm eating";
+  };
+
 }
 
-const newCat1 = new Cat ('Murzik','Red',5, true, 'Persian');
-const newCat2 = new Cat ('Sofa','White', 12 , false, 'Spinx');
-const newCat3 = new Cat ('Bernard','Red',3, true, null);
+const newCat1 = new Cat('Murzik', 'Red', 5, true, 'Persian');
+const newCat2 = new Cat('Sofa', 'White', 12, false, 'Spinx');
+const newCat3 = new Cat('Bernard', 'Red', 3, true, null);
